488a9f0f886bc9847c406bd708307bdfc7646435
Grey <sergejyurskyj@yahoo.com>
2020-01-29 22:05:42 -0800

    Committed Sizeof The project has been built for CLion

---
+++ 003_DataTypes GR/003_Samples/001_/001_Sizeof/001_Sizeof.cpp
@@ -0,0 +1,26 @@
+// oператор sizeof('тип [переменнаja]') в качестве параметра принимает тип, и возвращает количество байт памjaти занимаемое типом
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Bool: " << sizeof(bool) << endl;
+	cout << "Char: " << sizeof(char) << endl;
+	cout << "Short: " << sizeof(short) << endl;
+	cout << "Int: " << sizeof(int) << endl;
+	cout << "Long long: " << sizeof(long long) << endl;
+	cout << "Float: " << sizeof(float) << endl;
+	cout << "Double: " << sizeof(double) << endl;
+
+	int a = 4;
+
+	cout << sizeof a << endl;
+
+	// zадержка
+	cin.get();
+    return 0;
+}
+
